127a1984d478bda387f0688bfdc7409306e0b4ce
Advay Chandra <advay.chandra@protonmail.com>
2025-10-17 10:20:27 -0400

    Initialize motorRight in Motion class constructor

--- systems/mobility_system.py
+++ systems/mobility_system.py
@@ -4,3 +4,4 @@ class Motion:
   def __init__(self, **kwargs):
     motors = MotorPair(kwargs["motors"][0], kwargs["motors"][1])
     motorLeft = Motor(kwargs["motors"][0])
+    motorRight = Motor(kwargs["motors"][1])
