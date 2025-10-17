16dbeb666f4c585595631d53645a45a1683bcab7
Advay Chandra <advay.chandra@protonmail.com>
2025-10-17 10:19:55 -0400

    Initialize motorLeft in Motion class

--- systems/mobility_system.py
+++ systems/mobility_system.py
@@ -3,3 +3,4 @@ from buildhat import Motor, MotorPair
 class Motion:
   def __init__(self, **kwargs):
     motors = MotorPair(kwargs["motors"][0], kwargs["motors"][1])
+    motorLeft = Motor(kwargs["motors"][0])
