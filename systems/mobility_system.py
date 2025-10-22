528647f324e7ecaace030f3181d3976b90059ccb
Advay Chandra <advay.chandra@protonmail.com>
2025-10-22 12:12:53 -0400

    Add max_speed parameter to __init__ method
    Added max_speed parameter to mobility system initialization.

--- systems/mobility_system.py
+++ systems/mobility_system.py
@@ -5,3 +5,5 @@ class Motion:
     motors = MotorPair(kwargs["motors"][0], kwargs["motors"][1])
     motorLeft = Motor(kwargs["motors"][0])
     motorRight = Motor(kwargs["motors"][1])
+    max_speed = kwargs["max_speed"]
+    
