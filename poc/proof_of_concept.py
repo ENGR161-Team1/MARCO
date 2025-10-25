8be4acce015f17bf0aaab05b46b6246ccb9b17d5
AdvayRChandra <advay.chandra@protonmail.com>
2025-10-25 19:36:34 -0400

    Add LineFinder class and integrate it into proof_of_concept.py for line detection

--- poc/proof_of_concept.py
+++ poc/proof_of_concept.py
@@ -1,11 +1,24 @@
 from buildhat import MotorPair, Motor
+from basehat import LineFinder
 
 def main():
-    # Initialize the motor pair on ports A and B
-    motor_pair = MotorPair('A', 'B')
-    
-    # Move the motor pair forward for 2 seconds at 50% speed
-    motor_pair.start_timed(50, 2000)
+        # Initialize the motor pair on ports A and B
+        motor_pair = MotorPair('A', 'B')
+        line_finder_pin = 16
+        #grovepi.pinMode(line_finder_pin, "INPUT")
+
+        # Move the motor pair forward for 2 seconds at 50% speed
+        # motor_pair.start_timed(50, 2000)
+        linefinder = LineFinder(line_finder_pin)
+        while True:
+                sensor_value = linefinder.value
+                print("Line Finder Sensor Value:", sensor_value)
+                if sensor_value == 1:
+                        # print("On Track")
+                        motor_pair.start(-50, 50)  # Move forward
+                else:
+                        # print("Off Track")
+                        motor_pair.start(50, -50)  # Move backward
 
 if __name__ == "__main__":
     main()
