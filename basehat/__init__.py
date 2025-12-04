c3731c8494d637ed8d03f5aa963b5a09299310f1
AdvayRChandra <advay.chandra@protonmail.com>
2025-12-04 11:08:04 -0500

    Clean up basehat file naming: use snake_case, remove duplicate HallSensor.py

--- basehat/__init__.py
+++ basehat/__init__.py
@@ -2,10 +2,9 @@
 from .button import Button
 from .hall_sensor import HallSensor
 from .imu_sensor import IMUSensor
-from .UltrasonicSensor import UltrasonicSensor
+from .ultrasonic_sensor import UltrasonicSensor
 
 # Note: line_finder.py is currently empty - LineFinder class not yet implemented
-# Note: HallSensor.py is a duplicate of hall_sensor.py
 
 __all__ = [
     'Button',
